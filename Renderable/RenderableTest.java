a25c78535059c013e7cd38b45fd46310b7d835bf
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2017-03-27 15:29:38 +0700

    Junit testing

---
+++ Renderable/RenderableTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/**
+ * 
+ */
+
+/**
+ * @author Luthfi Fadillah
+ *
+ */
+public class RenderableTest implements Renderable{
+
+	private char test;
+
+	/**
+	 * Constructor dari RenderableTest.
+	 */
+	public RenderableTest(){
+		test = 'A';
+	}
+	
+	/**
+	 * Realisasi Method render().
+	 */
+	public char render(){
+		return 'A';
+	}
+	
+	/**
+	 * Test method for {@link Renderable#render()}.
+	 */
+	@Test
+	public void testRender() {
+		assertEquals("Method render Error!",test,render());
+	}
+
+}
