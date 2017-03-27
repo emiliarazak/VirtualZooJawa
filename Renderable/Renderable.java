287d52da961ae7fb49319bef6143f0434af9a019
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2017-03-27 15:28:39 +0700

    Renderable & Junit

---
+++ Renderable/Renderable.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author Luthfi Fadillah
+ * 
+ */
+
+public interface Renderable {
+  /** Virtual method Render untuk mengembalikan kode renderable.
+   * @return char : Mengembalikan kode renderable.
+   */
+  char render();
+
+}
