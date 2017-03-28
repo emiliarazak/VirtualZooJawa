2fdd27570a85f736a4e1d4a018a0a3ee755917f4
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2017-03-28 18:09:44 +0700

    Add files via upload

---
+++ MainZoo.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+package mainzoo;
+
+import driver.Driver;
+
+/**Main Class untuk menjalankan Virtual Zoo.
+ * @author Luthfi Fadillah
+ *
+ */
+
+public class MainZoo {
+  private static Driver mainzoo = new Driver();
+  /**Main method.
+   * @param args Do nothing
+   */
+  
+  public static void main(String[] args) {
+    mainzoo.MainMenu();
+  }
+
+}
